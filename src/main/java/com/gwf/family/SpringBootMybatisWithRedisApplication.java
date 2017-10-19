8dcabb8fe3cf9a6427000d365f03ec4208a89346
MarkGao11520 <1152057576@qq.com>
2017-10-19 21:17:42 +0800

    整理

--- src/main/java/com/gwf/family/SpringBootMybatisWithRedisApplication.java
+++ src/main/java/com/gwf/family/SpringBootMybatisWithRedisApplication.java
@@ -2,6 +2,8 @@ package com.gwf.family;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
